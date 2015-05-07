5aad74e101b6e29e92e2b65607cced09e9ee8a8f
Ian Henderson <ian@ianhenderson.org>
2015-05-07 11:21:02 -0700

    Center the text in the "run" button.

--- solitaryDongeon/ViewController.m
+++ solitaryDongeon/ViewController.m
@@ -168,6 +168,7 @@
 	_discardBar.frame = CGRectMake(0, 0, 0, 0);
 	
 	_runButton.backgroundColor = [UIColor blackColor];
+	_runButton.titleEdgeInsets = UIEdgeInsetsMake(2.5, 0, 0, 0);
 	_runButton.layer.cornerRadius = margin/4;
 	_runButton.layer.borderColor = [[UIColor whiteColor] CGColor];
 	_runButton.layer.borderWidth = 1;
